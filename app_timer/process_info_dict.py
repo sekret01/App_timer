d511ec480f021e247bfe3fce58d4e51c7b19bd02
Pavel <asinskijp188@gmail.com>
2024-11-25 19:41:14 +0300

    delete all functions for creating a list of processes from class 'process_info_dict'

--- app_timer/process_info_dict.py
+++ app_timer/process_info_dict.py
@@ -19,44 +19,11 @@ class ProcessInfoDict:
         self.settings: ProcessSetting = ProcessSetting()
         self.processes_list_creator = ProcessListCreator()
 
-    # create process name list
-
-    def get_all_processes(self) -> str:
-        """Get all information running processes (use cmd)"""
-        processes = subprocess.run(["tasklist", "/FO", "CSV", "/FI", "STATUS eq RUNNING", "/NH"],
-                                   capture_output=True)
-        return processes.stdout.decode('utf-8', errors='ignore')
-
-    def get_all_names_processes(self, processes: str) -> list:
-        """Take only names of processes"""
-        proc_info_list = processes.replace('\"', '').split('\n')
-        proc_names = [line.split(',')[0] for line in proc_info_list]
-        return proc_names
-
-    def filter_process_names(self, processes: list[str]) -> list:
-        """Removes all duplicates and ignored processes"""
-        process_set: set = set(processes)
-        process_names_list: list[str] = [proc for proc in process_set if self.no_ignore_process(proc)]
-        return process_names_list
-
-    def no_ignore_process(self, process) -> bool:
-        """Check process in ignore processes"""
-        with open(self.settings.ignore_process_path, "r", encoding='utf-8') as f:
-            ignore = f.read()
-            return True if process not in ignore else False
-
-    def get_processes_list(self) -> list[str]:
-        """create list with processes"""
-        processes_info: str = self.get_all_processes()
-        all_process_names: list[str] = self.get_all_names_processes(processes_info)
-        process_names = self.filter_process_names(all_process_names)
-        return process_names
-
     # update data
 
     def update_process_list(self) -> None:
         """Update list with process, adding new process"""
-        processes: list[str] = self.get_processes_list()
+        processes: list[str] = self.processes_list_creator.get_processes_list()
         for proc in processes:
             self.processes.setdefault(proc, AppTime())
         self.update_process_time(processes)
