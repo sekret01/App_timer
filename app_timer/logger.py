359aa285ceb64ada9be21486dbe812fc39a7b3dd
Pavel <asinskijp188@gmail.com>
2024-11-30 16:46:41 +0300

    first model of logger

--- app_timer/logger.py
+++ app_timer/logger.py
@@ -2,18 +2,24 @@ import logging
 import logging.config
 
 
-logging.config.fileConfig("logging.conf")
+logging.config.fileConfig("logger/logging.conf")
 
 
 class Logger:
     def __init__(self):
-        self.error_logger = logging.getLogger("ErrorLogger")
         self.main_logger = logging.getLogger("MainLogger")
+        self.error_logger = logging.getLogger("ErrorLogger")
+
 
     def log_info(self, msg: str):
         self.main_logger.info(msg=msg)
 
     def log_error(self, msg: str):
-        self.main_logger.error(msg=msg)
         self.error_logger.error(msg=msg)
 
+l = Logger()
+for i in range(-50, 50, 1):
+    if i%2:
+        l.log_info("all ok")
+    else:
+        l.log_error(f"{i} is not work")
